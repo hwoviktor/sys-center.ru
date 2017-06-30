6261ae850417d285d85963bf8a5a4c9b6691bd4b
RemiZOffAlex <remizoffalex@gmail.com>
2017-06-30 19:44:59 +0300

    Страница

--- www/index.php
+++ www/index.php
@@ -34,21 +34,46 @@ include 'header.php'
 
 <div class="container-fluid">
     <div class="row">
+        <div class="panel panel-default">
+            <div class="panel-heading">MikroTik, Ubiquiti, Linux, FreeBSD</div>
+<div class="panel-body">
         <div class="col-md-2">
             <img class="img-responsive" src="/static/images/logo.png" />
         </div>
         <div class="col-md-10">
-            <pre><code>----------------------------------------------
-MikroTik, Ubiquiti, Linux, FreeBSD
-----------------------------------------------
-Компьютерные сети - от новичка к профессионалу!
-Здесь мы будем учиться настраивать и правильно строить компьютерную сеть. Вести обсуждение оборудования разных производителей, рассмотрение технологий, топологий и решать возникшие трудности.</pre></code>
+<p>Компьютерные сети - от новичка к профессионалу!</p>
+<p>Здесь мы будем учиться настраивать и правильно строить компьютерную сеть. Вести обсуждение оборудования разных производителей, рассмотрение технологий, топологий и решать возникшие трудности.</p>
+</div>
+</div>
         </div>
     </div>
 
     <div class="row">
-
+        <div class="panel panel-default">
+            <div class="panel-heading">О нас</div>
+            <div class="panel-body">
+                <p>Мы - группа бойцов невидимого фронта.</p>
+            </div>
+        </div>
     </div>
+
+
+        <div class="row">
+            <div class="panel panel-default">
+                <div class="panel-heading">Партнёры</div>
+                <div class="panel-body">
+                    <a class="btn btn-default" href="https://justhost.ru/" target="_blank">JustHost.ru</a>
+                    <a class="btn btn-default" href="http://telecom-sales.ru/" target="_blank">telecom-sales.ru</a>
+                    <a class="btn btn-default" href="http://mikrotik-sibir.ru/" target="_blank">mikrotik-sibir.ru</a>
+                    <a class="btn btn-default" href="http://ru.blitzortung.org" target="_blank">Молния и гроза в реальном времени - Карты Google</a>
+                    <a class="btn btn-default" href="http://ubnt.com" target="_blank">Ubiquiti Networks</a>
+                    <a class="btn btn-default" href="http://eth0.me" target="_blank">твой IP</a>
+                    <a class="btn btn-default" href="http://mikrotik.com" target="_blank">MikroTik Routers and Wireless</a>
+                    <a class="btn btn-default" href="http://searchmac.com" target="_blank">SearchMac.com - vendor search by MAC | MAC address lookup service</a>
+                </div>
+            </div>
+        </div>
+
 </div>
 
 </body>
