881231d25621b704bd3a8aa3a151a111fb7b2c6b
RemiZOffAlex <remizoffalex@gmail.com>
2017-06-30 19:33:00 +0300

    Bootstrap

--- www/header.php
+++ www/header.php
@@ -1,4 +1,12 @@
+<?php
+
+?>
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-    <script src="/static/js/jquery-3.2.1.js') }}"></script>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/static/css/bootstrap.css" />
+    <link rel="stylesheet" href="/static/css/font-awesome.css" />
+    <link rel="stylesheet" href="/static/css/style.css" />
+    <script src="/static/js/jquery-2.2.4.js"></script>
+    <script src="/static/js/bootstrap.js"></script>
 </head>
--- www/index.php
+++ www/index.php
@@ -1,17 +1,55 @@
 <?php
 
-header('Location: http://sys-center.ru/');
 header('Content-Type: text/html; charset=utf-8');
 
 include 'config.php'
 
 ?>
-<html>
+<!DOCTYPE html>
+<html lang="ru">
 <?php
 
 include 'header.php'
 
 ?>
 <body>
+
+    <div class="navbar navbar-default">
+        <div class="container-fluid">
+        <div class="navbar-header">
+            <div class="pull-right">
+        <ul class="nav navbar-nav">
+          <li class="active"><a href="/index.php"><i class="fa fa-home"></i> На главную</a></li>
+          <li><a href="https://vk.com/mikrotik_os" target="_blank">Группа VK</a></li>
+          <li><a href="#">О нас</a></li>
+        </ul>
+        </div>
+        <div class="btn" style="color: #777777;">
+            <img src="/static/images/logo.png" width="27px" />
+            SYS-CENTER.RU
+       </div>
+      </div>
+        </div>
+    </div>
+
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-md-2">
+            <img class="img-responsive" src="/static/images/logo.png" />
+        </div>
+        <div class="col-md-10">
+            <pre><code>----------------------------------------------
+MikroTik, Ubiquiti, Linux, FreeBSD
+----------------------------------------------
+Компьютерные сети - от новичка к профессионалу!
+Здесь мы будем учиться настраивать и правильно строить компьютерную сеть. Вести обсуждение оборудования разных производителей, рассмотрение технологий, топологий и решать возникшие трудности.</pre></code>
+        </div>
+    </div>
+
+    <div class="row">
+
+    </div>
+</div>
+
 </body>
 </html>
